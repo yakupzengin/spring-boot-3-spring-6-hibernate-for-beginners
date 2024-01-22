df870f1e29700b2d0a97bad4d909ae6a1d417fe7
Yakup Zengin <clafedersin@gmail.com>
2024-01-22 11:47:56 +0300

    created constructor-injection-demo

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/test/java/com/luv2de/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2de.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
