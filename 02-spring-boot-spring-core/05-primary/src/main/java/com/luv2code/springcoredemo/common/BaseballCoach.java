2a487ee4f1b38116eb32452fbcf582a8f73596e1
Yakup Zengin <clafedersin@gmail.com>
2024-01-23 15:40:42 +0300

    added primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class BaseballCoach implements  Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice";
+    }
+}
