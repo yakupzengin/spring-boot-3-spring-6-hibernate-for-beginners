b5f38b41cead3dedbe14abbd651a68b8c6f7c1e4
Yakup Zengin <clafedersin@gmail.com>
2024-01-30 13:36:43 +0300

    added method with querying objects with JPA

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,13 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student student);
     Student findById(Integer id);
+
+    List<Student> findAll();
+    List<Student> findByLastName(String theLastName);
 }
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,10 +2,13 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.hibernate.boot.model.source.internal.hbm.AttributesHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -31,6 +34,29 @@ public class StudentDAOImpl implements StudentDAO{
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
     }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student" , Student.class);
+        // return query
+
+        return theQuery.getResultList() ;
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                                "From Student WHERE lastName=:theData" , Student.class);
+
+        // set query parameters
+        theQuery.setParameter("theData" , theLastName);
+
+        // return query result
+        return theQuery.getResultList();
+    }
+
 
 
 }
