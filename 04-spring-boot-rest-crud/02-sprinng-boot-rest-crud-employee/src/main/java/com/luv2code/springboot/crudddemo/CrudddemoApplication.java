3d152dcf161ecef24cce34cd43ad6de6913ed174
Yakup Zengin <clafedersin@gmail.com>
2024-02-09 01:47:11 +0300

    craeted spring boot rest crud employee project's files

---
+++ 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/CrudddemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.crudddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudddemoApplication.class, args);
+	}
+
+}
