6e955893bc8e9ecdda892a7a0df93edf599de604
Yakup Zengin <clafedersin@gmail.com>
2024-02-12 17:46:53 +0300

    Added methods Service :Add, Update, Delete

--- 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.crudddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.crudddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
