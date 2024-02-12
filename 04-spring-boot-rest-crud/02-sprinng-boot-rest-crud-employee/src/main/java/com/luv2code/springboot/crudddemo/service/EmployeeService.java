ab32a0b73138f8f6d15c8ebb4cba975492b96a90
Yakup Zengin <clafedersin@gmail.com>
2024-02-12 17:46:49 +0300

    Added methods Service :Add, Update, Delete

--- 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/service/EmployeeService.java
@@ -9,4 +9,7 @@ public interface EmployeeService {
     // Service Facade design pattern
 
     List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
 }
