de67eb42958977fa66ebf02467a5487a466f0315
Yakup Zengin <clafedersin@gmail.com>
2024-02-12 18:09:12 +0300

    Added method REST : Get single Employee

--- 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-sprinng-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/crudddemo/rest/EmployeeRestController.java
@@ -2,8 +2,10 @@ package com.luv2code.springboot.crudddemo.rest;
 
 import com.luv2code.springboot.crudddemo.entity.Employee;
 import com.luv2code.springboot.crudddemo.service.EmployeeService;
+import com.luv2code.springboot.crudddemo.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +28,18 @@ public class EmployeeRestController {
     public List<Employee> findAll () {
         return employeeService.findAll();
     }
+
+    // add mapping for GET /employee/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - "+ employeeId);
+        }
+        return theEmployee;
+    }
+
+
+
 }
