572fe0adca192c262baae50f197f0a52050cc696
Yakup Zengin <clafedersin@gmail.com>
2024-02-14 21:45:56 +0300

    used @RepositoryRestResource

--- 04-spring-boot-rest-crud/04-sprinng-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/crudddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-sprinng-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/crudddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springboot.crudddemo.dao;
 
 import com.luv2code.springboot.crudddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // Entity type : Employee
     // Primary Key : Integer
